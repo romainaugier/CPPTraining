c55975d532c6ad2adbfc456f700bc9dd1f35c86c
romainaugier <augier.romain@sfr.fr>
2025-05-24 15:59:06 +0200

    More work on NFA

--- 10_NFA/nfa.cpp
+++ 10_NFA/nfa.cpp
@@ -5,6 +5,8 @@
 #include <iterator>
 #include <iostream>
 #include <ranges>
+#include <cstdint>
+#include <stack>
 
 using State = std::uint64_t;
 using States = std::unordered_set<State>;
@@ -20,33 +22,75 @@ struct TransitionHash
 };
 
 using Transitions = std::unordered_map<Transition, States, TransitionHash>;
+using EpsilonTransitions = std::unordered_map<State, States>;
 
 class NFA
 {
     States _states;
     Alphabet _alphabet;
     Transitions _transitions;
+    EpsilonTransitions _epsilon_transitions;
 
-    State _start_state;
+    States _start_states;
     States _accept_states;
+
+    States _epsilon_closure(const States& states) const noexcept
+    {
+        States new_states = states;
+
+        std::stack<State> stack;
+
+        for(const auto& state : states)
+        {
+            stack.push(state);
+        }
+
+        while(!stack.empty())
+        {
+            State state = stack.top();
+            stack.pop();
+
+            auto it = this->_epsilon_transitions.find(state);
+
+            if(it != this->_epsilon_transitions.end())
+            {
+                for(const auto& next : it->second)
+                {
+                    if(new_states.insert(next).second)
+                    {
+                        stack.push(next);
+                    }
+                }
+            }
+        }
+
+        return new_states;
+    }
 
 public:
     NFA(States states,
         Alphabet alphabet,
         Transitions transitions,
-        State start_state,
+        EpsilonTransitions epsilon_transitions,
+        States start_states,
         States accept_states) : _states(std::move(states)),
                                 _alphabet(std::move(alphabet)),
                                 _transitions(std::move(transitions)),
-                                _start_state(start_state),
+                                _epsilon_transitions(std::move(epsilon_transitions)),
+                                _start_states(start_states),
                                 _accept_states(std::move(accept_states)) {}
 
     bool accepts(const std::string& str) const noexcept
     {
-        States current_states({ this->_start_state });
+        States current_states = this->_epsilon_closure(this->_start_states);
 
         for(const auto& s : str)
         {
+            if(!this->_alphabet.contains(s))
+            {
+                return false;
+            }
+
             States next_states;
 
             for(const auto& state : current_states)
@@ -61,21 +105,13 @@ public:
                 }
             }
 
-            current_states = std::move(next_states);
+            current_states = this->_epsilon_closure(next_states);
 
             if(current_states.empty())
             {
                 return false;
             }
         }
-
-        /*
-        std::vector<State> intersection;
-
-        std::set_intersection(current_states.begin(), current_states.end(), this->_accept_states.begin(), this->_accept_states.end(), std::back_inserter(intersection));
-
-        return intersection.size() > 0;
-        */
 
         return std::ranges::any_of(current_states, [this](State s) -> bool {
             return this->_accept_states.contains(s);
@@ -88,11 +124,14 @@ int main(int argc, char** argv)
     NFA nfa(States({ 0, 1, 2 }),
             Alphabet({ 'a', 'b' }),
             Transitions({ {{ 0, 'a' }, { 0, 1 }}, {{ 1, 'b' }, { 2 }}, {{ 2, 'a' }, { 2 }} }),
-            0,
+            EpsilonTransitions({ { 0, { 1 } } }),
+            States({ 0 }),
             States({ 2 }));
 
-    std::cout << "aab is accepted: " << nfa.accepts("aab") << "\n";
-    std::cout << "ab is accepted: " << nfa.accepts("ab") << "\n";
+    std::cout << "aab is accepted: " << nfa.accepts("aab") << "\n";   /* Should accept */
+    std::cout << "aaba is accepted: " << nfa.accepts("aaba") << "\n"; /* Should accept */
+    std::cout << "ab is accepted: " << nfa.accepts("ab") << "\n";     /* Should accept */
+    std::cout << "b is accepted: " << nfa.accepts("b") << "\n";       /* Should accept */
     std::cout << "aabb is accepted: " << nfa.accepts("aabb") << "\n";
     std::cout << "aa is accepted: " << nfa.accepts("aa") << "\n";
 
